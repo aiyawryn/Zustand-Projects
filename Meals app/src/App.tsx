72c0c8cf18e97bac35b18947eb637bc55d809c21
Aiya <110978849+aiyawryn@users.noreply.github.com>
2026-01-12 17:57:00 +0900

    commit meals app

---
+++ Meals app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Meals from "./components/Meals";
+
+const App = () => {
+  return (
+    <section>
+      <Meals />
+    </section>
+  );
+};
+
+export default App;
