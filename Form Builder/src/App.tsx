7166720db8ffc92760d379ffe7725b8e7b138ef3
Aiya <110978849+aiyawryn@users.noreply.github.com>
2026-01-14 17:08:47 +0900

    commit Form builder

---
+++ Form Builder/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormBuilder from "./components/FormBuilder";
+
+const App = () => {
+  return (
+    <div>
+      <FormBuilder />
+    </div>
+  );
+};
+
+export default App;
